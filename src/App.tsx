9f1394975fca02c669bf9867d575c3c5ffadb34c
rakibaakter <rakibaakter635@gmail.com>
2024-06-28 18:15:08 +0600

    handled increment with payload and defined ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,18 @@
-import { useDispatch, useSelector } from "react-redux"
-import { decrement, increment } from "./redux/features/counterSlice"
+
+import { decrement, increment, incrementByValue } from "./redux/features/counterSlice"
+import { useAppDispatch, useAppSelector } from "./redux/hook";
 
 
 function App() {
  
-  const dispatch = useDispatch() ;
-  const {count} = useSelector(state=> state.counter)
+  const dispatch = useAppDispatch() ;
+  const {count} = useAppSelector((state)=> state.counter)
 
   return (
    <div className="flex justify-center items-center h-screen w-full">
     <div className="flex gap-4 border border-sky-400 p-16">
       <button onClick={()=>dispatch(increment())} className="bg-green-400 text-white px-3 py-2 rounded-md ">Increment</button>
+      <button onClick={()=>dispatch(incrementByValue(5))} className="bg-sky-400 text-white px-3 py-2 rounded-md ">Increment by 5</button>
       <h2 className="text-2xl">{count}</h2>
       <button onClick={()=>dispatch(decrement())} className="bg-red-400 text-white px-3 py-2 rounded-md ">Decrement</button>
     </div>
